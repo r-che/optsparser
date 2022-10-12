aef6a4ef50948fdd594dd389c141e07af51e6e64
RomanChebotarev <RomanCh@artel.im>
2022-10-12 18:03:47 +0300

    Start write tests

--- optsparser.go
+++ optsparser.go
@@ -10,6 +10,9 @@ import (
 
 const lsJoinDefault = ", "
 
+// Auxiliary variable to avoid tests termination on Usage() function
+var usageDoExit = true
+
 type OptsParser struct {
 	flag.FlagSet
 	shToLong		map[string]string
@@ -325,7 +328,11 @@ func (p *OptsParser) Usage(errDescr ...string) {
 		// Print option help info
 		fmt.Fprint(p.Output(), p.descrLongOpt(f))
 	}
-	os.Exit(1)
+
+	// XXX This condition is not satisfied only in tests
+	if usageDoExit {
+		os.Exit(1)
+	}
 }
 
 func (p *OptsParser) nextSep() string {
