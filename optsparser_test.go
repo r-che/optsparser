47d904c81cc4d7a674761ab06c024f5b35b369e7
RomanChebotarev <RomanCh@artel.im>
2022-10-14 18:01:25 +0300

    testing - TestParser done

--- optsparser_test.go
+++ optsparser_test.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"testing"
 	"os"
-	"time"
 	"io"
+	"sort"
 )
 
 const (
@@ -17,18 +17,6 @@ func init() {
 	usageDoExit = false
 }
 
-type testOpts struct {
-	vBool		bool
-	vString		string
-	vInt		int
-	vInt64		int64
-	vFloat64	float64
-	vDuration	time.Duration
-	vUint		uint
-	vUint64		uint64
-	vVar		any
-}
-
 func TestParser(t *testing.T) {
 	// Save current value of os.Args because it will be replaced by test values
 	origArgs := make([]string, 0, len(os.Args))
@@ -38,29 +26,58 @@ func TestParser(t *testing.T) {
 		os.Args = origArgs
 	}()
 
-	for testN, test := range tests {
+	// Get tests names and sort them
+	names := make([]string, 0, len(tests))
+	for name := range tests {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+
+	// Run tests sorted by names
+	for _, testN := range names {
+		// Get test
+		test := tests[testN]
+
 		// Reset usage triggered flag
 		usageTriggered = false
 
 		// Make a buffer to catch parser's output
-		pOut := &bytes.Buffer{}
+		tOut := &bytes.Buffer{}
 
 		// Create new parser
 		p := NewParser(stubApp,	// application name
-			// TODO Required arguments
-		).SetOutput(pOut)
+			test.required...,
+		).SetOutput(tOut)
 
+		sepN := -1
+		sep := func() {
+			if v, ok := test.separators[sepN]; ok {
+				p.AddSeparator(v)
+			}
+			sepN++
+		}
 		to := testOpts{}
 
-		p.AddBool("bool-opt", "boolean value", &to.vBool, test.defaults.vBool)
-		p.AddString("string-opt", "string value", &to.vString, test.defaults.vString)
-		p.AddInt("int-opt", "int value", &to.vInt,  test.defaults.vInt)
-		p.AddInt64("int64-opt", "int64 value", &to.vInt64, test.defaults.vInt64)
-		p.AddFloat64("float64-opt", "float64 value", &to.vFloat64, test.defaults.vFloat64)
-		p.AddDuration("duration-opt", "duration value", &to.vDuration, test.defaults.vDuration)
-		p.AddUint("uint-opt", "uint value", &to.vUint, test.defaults.vUint)
-		p.AddUint64("uint64-opt", "uint64 value", &to.vUint64, test.defaults.vUint64)
-		// TODO p.AddVar("var-opt", "var value", &to.flag.Value)
+		sep()	//	[-1] some kind of additional description
+		sep()	//	[0]
+		p.AddBool("bool-opt|b", "boolean value", &to.vBool, test.defaults.vBool)
+		sep()	//	[1]
+		p.AddString("string-opt|s", "string value", &to.vString, test.defaults.vString)
+		sep()	//	[2]
+		p.AddInt("int-opt|i", "int value", &to.vInt,  test.defaults.vInt)
+		sep()	//	[3]
+		p.AddInt64("int64-opt|I", "int64 value", &to.vInt64, test.defaults.vInt64)
+		sep()	//	[4]
+		p.AddFloat64("float64-opt|f", "float64 value", &to.vFloat64, test.defaults.vFloat64)
+		sep()	//	[5]
+		p.AddDuration("duration-opt|d", "duration value", &to.vDuration, test.defaults.vDuration)
+		sep()	//	[6]
+		p.AddUint("uint-opt|u", "uint value", &to.vUint, test.defaults.vUint)
+		sep()	//	[7]
+		p.AddUint64("uint64-opt|U", "uint64 value", &to.vUint64, test.defaults.vUint64)
+		sep()	//	[8]
+		p.AddVar("var-ymd-opt|V", "var value", &to.vVar)
+		sep()	//	[9]
 
 		// Update test arguments to insert command name
 		test.args = append([]string{os.Args[0]}, test.args...)
@@ -76,13 +93,15 @@ func TestParser(t *testing.T) {
 			// Is it true?
 			if usageTriggered {
 				// False, Usage called
-				t.Errorf("[%d] parse failed: usage message - %#v, args - %#v", testN, pOut.String(), test.args)
+				t.Errorf("%q parse failed: args - %#v, test output:" +
+					"\n-------- Start output --------\n%s\n-------- End output --------",
+					testN, test.args, tOut.String())
 				continue
 			}
 
 			// Need to compare parsed and expected results
 			if to != test.want {
-				t.Errorf("[%d] incorrect Parse result: want - %#v got - %#v, args - %#v", testN, test.want, to, test.args)
+				t.Errorf("%q incorrect Parse result: want - %#v got - %#v, args - %#v", testN, test.want, to, test.args)
 				continue
 			}
 
@@ -92,7 +111,7 @@ func TestParser(t *testing.T) {
 
 		// Test should be failed
 		if !usageTriggered {
-			t.Errorf("[%d] incorrect Parse result - test should fail but it is successful; got - %#v, args - %#v",
+			t.Errorf("%q incorrect Parse result - test should fail but it succeeds; got - %#v, args - %#v",
 				testN, to, test.args)
 		}
 
--- test_data_test.go
+++ test_data_test.go
@@ -1,29 +1,53 @@
 package optsparser
 
 import (
+	"fmt"
+	"strings"
 	"time"
+	"strconv"
 )
 
-var tests = []struct{
+type testOpts struct {
+	vBool		bool
+	vString		string
+	vInt		int
+	vInt64		int64
+	vFloat64	float64
+	vDuration	time.Duration
+	vUint		uint
+	vUint64		uint64
+	vVar		testOptVarYMD
+}
+
+// Reference value
+var ref = testOpts{
+	vBool:		true,
+	vString:	`I think, therefore I am`,
+	vInt:		-430,
+	vInt64:		-59604644783353249,
+	vFloat64:	3.141592,
+	vDuration:	time.Hour * 4176,
+	vUint:		220414,
+	vUint64:	40208000000000,
+	vVar:		testOptVarYMD(20221014),
+}
+
+// Set of tests
+var tests = map[string]struct{
 	defaults	testOpts
+	required	[]string
 	args		[]string
 	want		testOpts
+	separators	map[int]string
 	needOK		bool
 }{
-	{
-		defaults: testOpts{
-			vBool:		false,
-			vString:	"",
-			vInt:		0,
-			vInt64:		0,
-			vFloat64:	0.0,
-			vDuration:	time.Second,
-			vUint:		0,
-			vUint64:	0,
-			// TODO vVar		any
-		},
+	//
+	// Test OK cases
+	//
+
+	// No required options, no special default values
+	`00[ok]no-required-no-defaults`: {
 		args: []string{
-			`--bool-opt=true`,
 			`--bool-opt`,
 			`--string-opt`,		`I think, therefore I am`,
 			`--int-opt`,		`-430`,								// Dead Sea level below sea level
@@ -32,19 +56,156 @@ var tests = []struct{
 			`--duration-opt`,	`250560m`,							// 4176 hours => 174 days
 			`--uint-opt`,		`220414`,							// The End and the Beginning
 			`--uint64-opt`,		`40208000000000`,					// distance between Sun and Proxima Centauri, km
-			// TODO `var-opt`, ``
+			`--var-ymd-opt`,	`2022.10.14`,
+			`command line arg#1`, `command line arg#2`, `command line arg#3`,
 		},
-		want: testOpts{
-			vBool:		true,
-			vString:	`I think, therefore I am`,
-			vInt:		-430,
-			vInt64:		-59604644783353249,
-			vFloat64:	3.141592,
-			vDuration:	time.Hour * 4176,
-			vUint:		220414,
-			vUint64:	40208000000000,
-			// TODO vVar		any
+		separators: map[int]string {
+			-1:	`## Start options list`,
+			0:	`# Boolean options`,
+			1:	`# String options`,
+			2:	`# Integer options`,
+			4:	`# Float options`,
+			5:	`# Duration options`,
+			6:	`# Unsigned int options`,
 		},
+		want: ref,	// expected result equal reference value
 		needOK:	true,
 	},
+
+	// No required options, with special default values
+	`01[ok]no-required-with-defaults`: {
+		defaults: ref,	// pass reference value as defaults
+		args: []string{
+			`--var-ymd-opt`,	`2022.10.14`,	// XXX AddVar() does not support default value, need to provide through arguments
+			`command line arg#1`, `command line arg#2`, `command line arg#3`,
+		},
+		want: ref,		// expect the same value after parsing
+		needOK:	true,
+	},
+
+	// Short options test - no required options, no special default values
+	`02[ok]sort-opts_no-required-no-defaults`: {
+		args: []string{
+			`-b`,
+			`-s`, `I think, therefore I am`,
+			`-i`, `-430`,							// Dead Sea level below sea level
+			`-I`, `-59604644783353249`,				// Leyland prime number
+			`-f`, `3.141592`,
+			`-d`, `250560m`,						// 4176 hours => 174 days
+			`-u`, `220414`,							// The End and the Beginning
+			`-U`, `40208000000000`,					// distance between Sun and Proxima Centauri, km
+			`-V`,`2022.10.14`,
+		},
+		want: ref,	// expected result equal reference value
+		needOK:	true,
+	},
+
+	// Test required options, no special default values
+	`03[ok]required-opts-no-defaults`: {
+		required: []string{
+			`bool-opt`,
+			`int64-opt`,
+			`duration-opt`,
+			`var-ymd-opt`,
+		},
+		args: []string{
+			`--bool-opt`,
+			`--string-opt`,		`I think, therefore I am`,
+			`--int-opt`,		`-430`,								// Dead Sea level below sea level
+			`--int64-opt`,		`-59604644783353249`,				// Leyland prime number
+			`--float64-opt`,	`3.141592`,
+			`--duration-opt`,	`250560m`,							// 4176 hours => 174 days
+			`--uint-opt`,		`220414`,							// The End and the Beginning
+			`--uint64-opt`,		`40208000000000`,					// distance between Sun and Proxima Centauri, km
+			`--var-ymd-opt`,	`2022.10.14`,
+			// Test work without command line arguments
+		},
+		want: ref,	// expected result equal reference value
+		needOK:	true,
+	},
+
+	//
+	// Test FAIL cases
+	//
+	// TODO
+
+	// Incorect option passed
+	`10[fail]incorrect-option`: {
+		args: []string{
+			`--bool-opt`,
+			`--string-opt`,		`I think, therefore I am`,
+			`--int-opt`,		`-430`,								// Dead Sea level below sea level
+			`--int64-opt`,		`-59604644783353249`,				// Leyland prime number
+			`--float64-opt`,	`3.141592`,
+			`--special-incorrect-option`,
+			`--duration-opt`,	`250560m`,							// 4176 hours => 174 days
+			`--uint-opt`,		`220414`,							// The End and the Beginning
+			`--uint64-opt`,		`40208000000000`,					// distance between Sun and Proxima Centauri, km
+			`--var-ymd-opt`,	`2022.10.14`,
+			`command line arg#1`, `command line arg#2`, `command line arg#3`,
+		},
+		want: ref,	// expected result equal reference value
+		needOK:	false,
+	},
+
+	// Incorrect values passed - all Parse have to fail
+	`11[fail]incorrect-option-bool`:		{ args: []string{`--bool-opt=invalid`}, want: testOpts{} },
+	// XXX Skip testing the string type because it is impossible to pass something inappropriate for the string type
+	`12[fail]incorrect-option-int`:			{ args: []string{`--int-opt`, `f430`}, want: testOpts{} },
+	`13[fail]incorrect-option-int64`:		{ args: []string{`--int64-opt`, `59604644783353249.1`}, want: testOpts{} },
+	`14[fail]incorrect-option-float64`:		{ args: []string{`--float64-opt`, `3.G+e0`, }, want: testOpts{} },
+	`15[fail]incorrect-option-duration`:	{ args: []string{`--duration-opt`, `1Y`}, want: testOpts{} },
+	`16[fail]incorrect-option-uint`:		{ args: []string{`--uint-opt`, `-220414`}, want: testOpts{} },
+	`17[fail]incorrect-option-uint64`:		{ args: []string{`--uint64-opt`, `-40208000000000`}, want: testOpts{} },
+	`18[fail]incorrect-option-var`:			{ args: []string{`--var-ymd-opt`, `2022/10/14`, }, want: testOpts{} },
+
+	// Test required option missing
+	`19[fail]required-opts`: {	// TODO fix number of test
+		required: []string{
+			`bool-opt`,
+			`int64-opt`,
+			`duration-opt`,
+			`var-ymd-opt`,
+		},
+		args: []string{
+			`--bool-opt`,
+			`--string-opt`,		`I think, therefore I am`,
+			`--int-opt`,		`-430`,								// Dead Sea level below sea level
+			`--int64-opt`,		`-59604644783353249`,				// Leyland prime number
+			`--float64-opt`,	`3.141592`,
+			`--uint-opt`,		`220414`,							// The End and the Beginning
+			`--uint64-opt`,		`40208000000000`,					// distance between Sun and Proxima Centauri, km
+			`--var-ymd-opt`,	`2022.10.14`,
+			`command line arg#1`, `command line arg#2`, `command line arg#3`,
+		},
+		want: ref,	// expected result equal reference value
+		needOK:	false,
+	},
+}
+
+//
+// Type to test AddVar() - parses date in format YYYY.MM.DD
+//
+
+type testOptVarYMD int
+func (v *testOptVarYMD) String() string {
+	return fmt.Sprintf("Year %d month %d day %d", *v / 10000, (*v % 10000) / 100, *v % 100)
+}
+func (ov *testOptVarYMD) Set(val string) error {
+	ymd := make([]int, 0, 3)
+	for _, part := range strings.Split(val, ".") {
+		v, err := strconv.ParseInt(part, 10, 32)
+		if err != nil {
+			return fmt.Errorf("unparsable entry %q in date - %v", part, err)
+		}
+		ymd = append(ymd, int(v))
+	}
+	// Check length
+	if len(ymd) != 3 {
+		return fmt.Errorf("invalid YMD date length, want - 3, got - %d", len(ymd))
+	}
+	// Set decimal integer value
+	*ov = testOptVarYMD(ymd[0] * 10000 + ymd[1] * 100 + ymd[2])
+	// OK
+	return nil
 }
